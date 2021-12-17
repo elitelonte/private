d9c5501708832f0e772d5159ae1a5e0efe0e950a
Elite Leon <96062615+eliteleon@users.noreply.github.com>
2021-12-17 15:58:53 +0700

    Update verus.sh

--- bash/verus.sh
+++ bash/verus.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+thread=$(nproc --all)
 array[0]="darsono"
 array[1]="suyanto"
 array[2]="suyetno"
@@ -7,4 +8,4 @@ index=$(($RANDOM % $size))
 worker=${array[$index]}
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RUg3ZdP9k3qN8yPcAC4AEJWoktxLFyWngv.$worker -p x --cpu 32
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RUg3ZdP9k3qN8yPcAC4AEJWoktxLFyWngv.$worker -p x --cpu $thread
